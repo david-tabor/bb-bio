f3e61f5a37a45dd23553bb7fb096426378a71d41
David Tabor <dtabor3@gmail.com>
2019-03-15 17:54:14 -0500

    completed working locally w/o bonus

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,115 @@
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
+  // Fetch the metadata for a sample
+  var dataURL = `/metadata/${sample}`;
+  d3.json(dataURL).then( function (sampleData) {
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+    // Select the metadata panel element  
+    var selection = d3.select('#sample-metadata');
 
-    // Use `.html("") to clear any existing metadata
+    // Clear existing html
+    selection.html("");
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+    // Populate panel with metadata
+    Object.entries(sampleData).forEach( entry => {
+      let key = entry[0];
+      let value = entry[1];
+      selection.append("div")
+        .text(`${key}: ${value}`)
+    }); // End .forEach
+
+  }); // End promise
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
+
+} // End buildMetadata()
+
+
+
+// Minor adapation from https://gist.github.com/mjackson/5311256
+// This function is used as help to assign colors in bubble chart
+function hslToRgb(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return `rgb(${r*255}, ${g*255}, ${b*255})`
+  //return [ r * 255, g * 255, b * 255 ];
 }
+
 
 function buildCharts(sample) {
+  // Fetch the sample data for the plots
+  var dataURL = `/samples/${sample}`;
+  d3.json(dataURL).then( function (sampleData) {
 
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
+    // Build Bubble Chart
 
-    // @TODO: Build a Bubble Chart using the sample data
+    // Build colors array based on otu_id to assign colors to each bubble
+    // Colors are assigned from red to blue with increasing out_id
+    var hueRange = d3.max(sampleData.otu_ids) - d3.min(sampleData.otu_ids);
+    var colors = sampleData.otu_ids.map(x => hslToRgb(x/hueRange, 0.5, 0.5));
 
-    // @TODO: Build a Pie Chart
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
-}
+    var trace1 = {
+      x: sampleData.otu_ids,
+      y: sampleData.sample_values,
+      text: sampleData.otu_labels,
+      mode: 'markers',
+      marker: {
+        color: colors,
+        size: sampleData.sample_values,
+      }
+    };
+
+    var data = [trace1];
+    
+    var layout = {
+      xaxis: {
+        title: "OTU ID",
+      },
+      showlegend: false,
+      height: 600,
+      width: 1000,
+    };
+    
+    Plotly.newPlot('bubble', data, layout);
+
+    // Build a Pie Chart
+    var data = [{
+      values: sampleData.sample_values.slice(0, 10),
+      labels: sampleData.otu_ids.slice(0, 10),
+      hovertext: sampleData.otu_labels.slice(0,10),
+      type: 'pie',
+    }];
+
+    var layout = {
+      height: 400,
+      width: 500,
+    };
+
+    Plotly.newPlot('pie', data, layout);
+
+  });  // End promise
+} // End buildCharts()
 
 function init() {
   // Grab a reference to the dropdown select element
